f123973c204bc54fcbe32ee23bda4935065e2867
vvbakhanovich <vv.bakh@yandex.ru>
2024-05-07 15:28:15 +0300

    feat: Add ShortCommentDto

--- ewm-service/src/main/java/ru/practicum/yandex/events/mapper/CommentMapper.java
+++ ewm-service/src/main/java/ru/practicum/yandex/events/mapper/CommentMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.practicum.yandex.events.dto.AddCommentDto;
 import ru.practicum.yandex.events.dto.CommentDto;
+import ru.practicum.yandex.events.dto.ShortCommentDto;
 import ru.practicum.yandex.events.dto.UpdateCommentDto;
 import ru.practicum.yandex.events.model.Comment;
 import ru.practicum.yandex.user.mapper.UserMapper;
@@ -19,6 +20,8 @@ public interface CommentMapper {
 
     @Mapping(source = "commentId", target = "id")
     Comment toModel(UpdateCommentDto updateCommentDto);
+
+    ShortCommentDto toShortDto(Comment comment);
 
     List<AddCommentDto> toDtoList(List<Comment> comments);
 }
